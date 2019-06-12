18141d020bbd5196ad4180ab665dcad5ddef31ce
TimeEscaper <rentgeny05@gmail.com>
2019-06-12 22:11:26 +0300

    Depth and yaw steering implemented

--- src/stingray_steering_lib/src/Steerer.cpp
+++ src/stingray_steering_lib/src/Steerer.cpp
@@ -1,8 +1,13 @@
 #include "Steerer.h"
 
-#include <ros/ros.h>
+#include <std_msgs/UInt32.h>
+#include <std_msgs/Int32.h>
 
-Steerer::Steerer(double linearSlow, double linearMedium, double linearFast, double linearTurbo) {
+#include <stingray_msgs/SetFloat64.h>
+#include <stingray_msgs/SetInt32.h>
+
+Steerer::Steerer(const ros::NodeHandle& nodeHandle, double linearSlow, double linearMedium,
+    double linearFast, double linearTurbo) : nodeHandle(nodeHandle) {
   linearVelocityValues[LinearVelocityLevel::LINEAR_LEVEL_SLOW] = linearSlow;
   linearVelocityValues[LinearVelocityLevel::LINEAR_LEVEL_MEDIUM] = linearMedium;
   linearVelocityValues[LinearVelocityLevel::LINEAR_LEVEL_FAST] = linearFast;
@@ -63,4 +68,44 @@ void Steerer::stop() {
   lagAndMarch.request.march = 0.0;
   lagAndMarch.request.lag = 0.0;
   ros::service::call(SET_LAG_AND_MARCH_SERVICE, lagAndMarch);
+}
+
+void Steerer::diveToDepth(int depthCm) {
+  bool depthReached = false;
+  boost::function<void (const std_msgs::UInt32::ConstPtr&)> callback =
+      [&depthReached, depthCm](const std_msgs::UInt32::ConstPtr& depthMessage) {
+    if (!depthReached) {
+      int currentDepth = static_cast<int>((*depthMessage).data);
+      depthReached = std::abs(depthCm - currentDepth) < DEPTH_DELTA;
+    }
+  };
+
+  // TODO: Add error handling
+  stingray_msgs::SetInt32 depthToSet;
+  depthToSet.request.value = depthCm;
+  ros::service::call(SET_DEPTH_SERVICE, depthToSet);
+
+  ros::Subscriber sub = nodeHandle.subscribe(DEPTH_SUBSCRIBE_TOPIC, 1, callback);
+  while (!depthReached);
+  sub.shutdown();
+}
+
+void Steerer::rotateToAngle(int angleDegree) {
+  bool yawReached = false;
+  boost::function<void (const std_msgs::Int32::ConstPtr&)> callback =
+      [&yawReached, angleDegree](const std_msgs::Int32::ConstPtr& yawMessage) {
+        if (!yawReached) {
+          int currentYaw = (*yawMessage).data;
+          yawReached = std::abs(angleDegree - currentYaw) < YAW_DELTA;
+        }
+  };
+
+  // TODO: Add error handling
+  stingray_msgs::SetInt32 yawToSet;
+  yawToSet.request.value = angleDegree;
+  ros::service::call(SET_YAW_SERVICE, yawToSet);
+
+  ros::Subscriber sub = nodeHandle.subscribe(YAW_SUBSCRIBE_TOPIC, 1, callback);
+  while (!yawReached);
+  sub.shutdown();
 }
