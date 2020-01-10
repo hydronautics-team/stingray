8e2dff3e6427a1150b23f844b18d3e1ca396dc3c
Davt2000 <davtart2000@yandex.ru>
2020-01-10 14:17:58 +0300

    Started creating demo scenario
    Signed-off-by: Davt2000 <davtart2000@yandex.ru>

---
+++ src/stingray_pilot/src/test_sequence_1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import rospy
+from stingray_msgs.srv import SetDeviceAction
+
+
+def scenario1():
+    pass
+
+if __name__ == '__main__':
+    try:
+        scenario1()
+    except rospy.ROSInterruptException: pass
