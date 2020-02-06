c19c59bbffb51f4a8b06b313861123e7cb0c5c4d
nikkkkkkkk <nikolyashiman@gmail.com>
2020-02-06 13:50:26 +0300

    Tack movement implemented

--- src/stingray_movement/include/patterns/TackServer.h
+++ src/stingray_movement/include/patterns/TackServer.h
@@ -1,6 +1,21 @@
 #ifndef STINGRAY_SRC_STINGRAY_MOVEMENT_INCLUDE_PATTERNS_TACKSERVER_H_
 #define STINGRAY_SRC_STINGRAY_MOVEMENT_INCLUDE_PATTERNS_TACKSERVER_H_
 
+#include <stingray_movement_msgs/Tack.h>
+
+#include <basic/LinearMovementServer.h>
+
 // TODO: Implement
+
+class TackServer : AbstractMovementActionServer<stingray_movement_msgs::TackAction,
+						stingray_movement_msgs::TackGoalConstPtr>
+{
+protected:
+	coid goalCallback(const stingray_movement_msgs::TackGoalConstPtr& goal) override;
+
+public:
+	TackServer(const const std::string& actionName, double velocityCoefficient);
+	~TackServer() = default;
+}
 
 #endif //STINGRAY_SRC_STINGRAY_MOVEMENT_INCLUDE_PATTERNS_TACKSERVER_H_
