e629df37b5a386137043f4830c3140bcc6ab33be
TimeEscaper <rentgeny05@gmail.com>
2020-02-08 05:01:18 +0300

    Alignment action implemented

--- src/stingray_movement/src/movement_patterns.cpp
+++ src/stingray_movement/src/movement_patterns.cpp
@@ -1,12 +1,29 @@
-#include <ros/ros.h>
+#include <patterns/AlignServer.h>
 
 static const std::string NODE_NAME = "movement_patterns";
+
+static const std::string PARAM_VELOCITY_COEFFICIENT = "velocity_coefficient";
+
+static const std::string ACTION_ALIGN = "stingray_action_align";
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, NODE_NAME);
   ros::NodeHandle nodeHandle(NODE_NAME);
 
-  // TODO: Instantiate movement patterns servers
+  double velocityCoefficient = 0.0;
+
+  bool hasParameter = nodeHandle.param(PARAM_VELOCITY_COEFFICIENT, velocityCoefficient, 0.0);
+  if (!hasParameter) {
+    ROS_ERROR("Parameter %s is not specified!", PARAM_VELOCITY_COEFFICIENT.c_str());
+    return 0;
+  }
+
+  if (velocityCoefficient < 1.0) {
+    ROS_ERROR("Velocity coefficient must be larger than 1.0");
+    return 0;
+  }
+
+  AlignServer linearMovementServer(ACTION_ALIGN, velocityCoefficient);
 
   ros::spin();
 
