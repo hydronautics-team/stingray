e2f80e9938fe4fa608458d41e22c2d9622044ab1
TimeEscaper <rentgeny05@gmail.com>
2020-02-22 07:58:56 +0300

    Lag stabilization support added to Stingray

--- src/stingray_drivers/src/hardware_bridge_nodelet.cpp
+++ src/stingray_drivers/src/hardware_bridge_nodelet.cpp
@@ -113,8 +113,11 @@ bool hardware_bridge::stabilizationCallback(stingray_msgs::SetStabilization::Req
     setStabilizationState(requestMessage, SHORE_STABILIZE_DEPTH_BIT, stabilizationRequest.depthStabilization);
     NODELET_DEBUG("Setting yaw stabilization %d", stabilizationRequest.yawStabilization);
     setStabilizationState(requestMessage, SHORE_STABILIZE_YAW_BIT, stabilizationRequest.yawStabilization);
-    depthStabilizationEnabled = true;
-    yawStabilizationEnabled = true;
+    NODELET_DEBUG("Setting lag stabilization %d", stabilizationRequest.lagStabilization);
+    setStabilizationState(requestMessage, SHORE_STABILIZE_LAG_BIT, stabilizationRequest.lagStabilization);
+    depthStabilizationEnabled = stabilizationRequest.depthStabilization;
+    yawStabilizationEnabled = stabilizationRequest.yawStabilization;
+    lagStabilizationEnabled = stabilizationRequest.lagStabilization;
 
     isReady = true;
     stabilizationResponse.success = true;
