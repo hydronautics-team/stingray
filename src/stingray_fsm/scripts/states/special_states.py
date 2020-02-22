6eed77f26d3a72a561e9f4e73acd1c5b2427c3bd
TimeEscaper <rentgeny05@gmail.com>
2020-02-22 17:52:02 +0300

    Red flare bypassing implemented; fixes in Stingray initialization

--- src/stingray_fsm/scripts/states/special_states.py
+++ src/stingray_fsm/scripts/states/special_states.py
@@ -12,11 +12,14 @@ class InitializationState(smach.State):
         self.delayAfterInit_ = delay_after_init
 
     def execute(self, userdata):
+        rospy.loginfo("Resetting IMU...")
         service = rospy.ServiceProxy("/stingray/services/control/set_imu_enabled", SetBool)
         response = service(True)
+        rospy.loginfo("IMU has been reset!")
         rospy.sleep(self.delayAfterInit_ / 1000.0)
         service = rospy.ServiceProxy("/stingray/services/control/set_stabilization", SetStabilization)
-        response = service(True, True)
+        response = service(True, True, False)
+        rospy.loginfo("Stabilization enabled")
         return "INIT_OK"
 
 
