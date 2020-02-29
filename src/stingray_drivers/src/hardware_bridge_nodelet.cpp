8a5fe760a98ea29f9cc3bd989fd4b4f8b1c85752
TimeEscaper <rentgeny05@gmail.com>
2020-02-29 18:28:24 +0300

    Lag stabilization enabled

--- src/stingray_drivers/src/hardware_bridge_nodelet.cpp
+++ src/stingray_drivers/src/hardware_bridge_nodelet.cpp
@@ -56,8 +56,15 @@ void hardware_bridge::inputMessage_callback(const std_msgs::UInt8MultiArrayConst
 
 bool hardware_bridge::lagAndMarchCallback(stingray_msgs::SetLagAndMarch::Request& lagAndMarchRequest,
                                           stingray_msgs::SetLagAndMarch::Response& lagAndMarchResponse) {
-    requestMessage.march = static_cast<int16_t> (lagAndMarchRequest.march);
-    requestMessage.lag = static_cast<int16_t> (lagAndMarchRequest.lag);
+
+    if (lagStabilizationEnabled) {
+      requestMessage.march = static_cast<int16_t> (0.0);
+      requestMessage.lag = static_cast<int16_t> (0.0);
+      requestMessage.lag_error = static_cast<int16_t> (lagAndMarchRequest.lag);
+    } else {
+      requestMessage.march = static_cast<int16_t> (lagAndMarchRequest.march);
+      requestMessage.lag = static_cast<int16_t> (lagAndMarchRequest.lag);
+    }
 
     isReady = true;
     lagAndMarchResponse.success = true;
@@ -113,8 +120,8 @@ bool hardware_bridge::stabilizationCallback(stingray_msgs::SetStabilization::Req
     setStabilizationState(requestMessage, SHORE_STABILIZE_DEPTH_BIT, stabilizationRequest.depthStabilization);
     NODELET_DEBUG("Setting yaw stabilization %d", stabilizationRequest.yawStabilization);
     setStabilizationState(requestMessage, SHORE_STABILIZE_YAW_BIT, stabilizationRequest.yawStabilization);
-    //NODELET_DEBUG("Setting lag stabilization %d", stabilizationRequest.lagStabilization);
-    //setStabilizationState(requestMessage, SHORE_STABILIZE_LAG_BIT, stabilizationRequest.lagStabilization);
+    NODELET_DEBUG("Setting lag stabilization %d", stabilizationRequest.lagStabilization);
+    setStabilizationState(requestMessage, SHORE_STABILIZE_LAG_BIT, stabilizationRequest.lagStabilization);
     depthStabilizationEnabled = stabilizationRequest.depthStabilization;
     yawStabilizationEnabled = stabilizationRequest.yawStabilization;
     lagStabilizationEnabled = stabilizationRequest.lagStabilization;
