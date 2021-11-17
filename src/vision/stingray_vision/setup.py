1566e8c93065f73436c2898fd8ec8734843598fe
Vladislav Plotnikov <v.plotnikov@mcc.vniiem.ru>
2021-11-17 17:40:30 +0300

    start refactoring

--- src/vision/stingray_vision/setup.py
+++ src/vision/stingray_vision/setup.py
@@ -1,12 +1,27 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+from setuptools import setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'stingray_vision'
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=[''],
-    package_dir={'': 'scripts'},
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='vladushked',
+    maintainer_email='vladushked@yandex.ru',
+    description='TODO: Package description',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = py_pubsub.publisher_member_function:main',
+            'listener = py_pubsub.subscriber_member_function:main',
+        ],
+    },
 )
-
-setup(**setup_args)
