21324fa9afde74f70d02c07e8a34b544b00a6a19
Davt2000 <davtart2000@yandex.ru>
2022-06-04 20:40:07 +0300

    reviewed old pilot and fsm
    Signed-off-by: Davt2000 <davtart2000@yandex.ru>

--- src/movement/stingray_pilot/scripts/gazebo_test_module.py
+++ src/movement/stingray_pilot/scripts/gazebo_test_module.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
 
-from top_level_actions import AUV
+from stingray_pilot.top_level_actions import AUV
 import rospy
+
 
 def main():
     rospy.init_node("action_test")
 
     auv = AUV()
 
-    # auv.forward_locked(1000, 0.3)      # initial move from base position
-    # auv.dive(95)
-    #
-    # # making a hexagonal "circle". TODO test this in water
-    # auv.circle(1500)
-    #
-    # auv.rotate(-180)            # return to base position
-    # auv.forward_locked(1000, 0.3)
-    # auv.rotate(180)
+    auv.forward_locked(1000, 0.3)      # initial move from base position
+    auv.dive(95)
+
+    # making a hexagonal "circle". TODO test this in water
+    auv.circle(1500)
+
+    auv.rotate(-180)            # return to base position
+    auv.forward_locked(1000, 0.3)
+    auv.rotate(180)
 
     # making a dodecagon "circle"
-    auv.execute_pattern("circle", 1000, 0.4, True, False)
+    # auv.execute_pattern("circle", 1000, 0.4, True, False)
+    #auv.circle(radius=2000)
 
 
 if __name__ == '__main__':
--- src/movement/stingray_pilot/setup.py
+++ src/movement/stingray_pilot/setup.py
@@ -5,8 +5,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=[''],
-    package_dir={'': 'scripts'},
+    packages=['stingray_pilot'],
+    package_dir={'': 'src'},
 )
 
 setup(**setup_args)
