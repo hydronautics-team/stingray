e33291d2ecc1e36e3a187e007f184a6ebdd87279
Grisha Starykh <grishafv@gmail.com>
2022-07-30 19:05:06 +0300

    try to qr with no global

--- src/utils/stingray_startup/scripts/qr_trigger.py
+++ src/utils/stingray_startup/scripts/qr_trigger.py
@@ -3,91 +3,61 @@
 import rospy
 import roslaunch
 import std_msgs.msg
-import std_srvs.srv
 
+class QrTrigger:
+    def __init__(self):
+        self.launched = False
+        self.callbackRequested = False
+        self.requestedMode = None
+        self.launch = None
+        rospy.loginfo('__init__ QrTrigger')
+        rospy.Subscriber('/barcode', std_msgs.msg.String, self.barcode_callback)
 
-launched = False
-requestedMode = None
-launchRequested = False
-stopRequested = False
-
-
-def barcode_callback(msg):
-    global launched
-    global launchRequested
-    global stopRequested
-    global requestedMode
-
-    message = msg.data.lower()
-    if not (message in ['demo', 'stop', 'qualification', 'simple', 'medium', 'medium_ha']):
-        rospy.logerr('Unknown messages')
-        return
-
-    if message == 'stop':
-        if not launched:
-            rospy.logwarn('Not launched yet')
+    def barcode_callback(self, msg):
+        rospy.loginfo('barcode_callback')
+        if self.callbackRequested == True:
             return
-        if stopRequested:
-            rospy.logwarn('Stop is already requested')
+        else:
+            self.callbackRequested == True
+
+        message = msg.data.lower()
+
+        if not (message in ['demo', 'stop', 'qualification', 'simple', 'medium', 'medium_ha']):
+            rospy.logerr('Unknown messages')
             return
-        stopRequested = True
+
+        if message == 'stop':
+            if not self.launched:
+                rospy.logwarn('Not launched yet')
+                return
+            else:
+                rospy.loginfo('Shutting down launch config...')
+                self.launch.shutdown()
+                self.launched = False
+                return
+
+        if self.launched:
+            rospy.logwarn('Already launched')
+            return
+
+        # uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+        # roslaunch.configure_logging(uuid)
+        # launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/grigorian/Hydro/sauvc/src/sauvc_startup/launch/sauvc_" +
+        #                                                  message + ".launch"]) #TODO: установить унивирсальный путь
+        # rospy.loginfo('Starting launch mode ' + message)
+        # launch.start()
+        rospy.loginfo('start ' + message)
+
+        self.launched == True
+        self.callbackRequested == False
+
         return
-
-    if launched:
-        rospy.logwarn('Already launched')
-        return
-    if launchRequested:
-        rospy.logwarn('Launch is already requested')
-        return
-
-    requestedMode = message
-    launchRequested = True
-
-
-def stop_notification_callback(req):
-    global stopRequested
-    stopRequested = True
-    return std_srvs.srv.TriggerResponse(True, 'OK')
-
-def main():
-    global launched
-    global launchRequested
-    global stopRequested
-    global requestedMode
-
-    rospy.init_node('qr_trigger')
-
-    launch = None
-
-    rospy.Subscriber('/barcode', std_msgs.msg.String, barcode_callback)
-    rospy.Service('global_fsm_finished', std_srvs.srv.Trigger, stop_notification_callback) # я не знаю что это за строчка
-
-    rate = rospy.Rate(1)
-
-    while not rospy.is_shutdown():
-        if launchRequested:
-            rospy.loginfo('while not rospy.is_shutdown ' + requestedMode)
-            uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-            roslaunch.configure_logging(uuid)
-            launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/grigorian/Hydro/sauvc/src/sauvc_startup/launch/sauvc_" +
-                                                             requestedMode + ".launch"]) #TODO: установить унивирсальный путь
-            rospy.loginfo('Starting launch mode ' + requestedMode)
-            launch.start()
-            launched = True
-            launchRequested = False
-
-        elif stopRequested:
-            rospy.loginfo('Shutting down launch config...')
-            launch.shutdown()
-            launched = False
-            stopRequested = False
-
-        rate.sleep()
-
-
 
 if __name__ == '__main__':
     try:
-        main()
+        rospy.init_node('qr_trigger')
+        qr = QrTrigger()
+        rospy.spin()
+
     except rospy.ROSInterruptException:
         pass
