b10ea4df22f42a71fdad940cbacd33b434e857a2
Vladislav Plotnikov <vladik1209@gmail.com>
2022-08-11 20:08:28 +0300

    fix topic

--- src/vision/stingray_object_detection/scripts/stingray_object_detection/utils.py
+++ src/vision/stingray_object_detection/scripts/stingray_object_detection/utils.py
@@ -3,7 +3,7 @@ from stingray_resources.utils import load_config
 
 
 def get_objects_topic(camera_topic: str) -> str:
-    return "%s%s/objects" % (camera_topic, load_config()["nodes"]["object_detection"])
+    return "%s/%s/objects" % (camera_topic, load_config()["nodes"]["object_detection"])
 
 def get_debug_image_topic(camera_topic: str) -> str:
-    return "%s%s/image" % (camera_topic, load_config()["nodes"]["object_detection"])
+    return "%s/%s/image" % (camera_topic, load_config()["nodes"]["object_detection"])
