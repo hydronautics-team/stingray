1a01046a5f9ea0910dfc7ed48dbbfc025e8411b4
Davt2000 <davtart2000@yandex.ru>
2022-09-13 01:58:29 +0300

    added tracking to vision (ObjectDetected) now it remembers the coordinates of center of detected object
    Signed-off-by: Davt2000 <davtart2000@yandex.ru>

--- src/missions/stingray_tfsm/scripts/stingray_tfsm/vision_events.py
+++ src/missions/stingray_tfsm/scripts/stingray_tfsm/vision_events.py
@@ -36,16 +36,19 @@ def get_best_object(objects, target_name, req_confidence):
     obj_to_num = list(enumerate([obj.name for obj in objects]))
     ii = -1
     ii_conf = -1
-    for i, name in obj_to_num:
-        if target_name == name:
-            if objects[i].confidence >= req_confidence and objects[i].confidence >= ii_conf:
-                ii = i
-                ii_conf = objects[i].confidence
-    if ii > -1:
-        _obj = objects[ii]
+    if obj_to_num:
+        for i, name in obj_to_num:
+            if target_name == name:
+                if objects[i].confidence >= req_confidence and objects[i].confidence >= ii_conf:
+                    ii = i
+                    ii_conf = objects[i].confidence
+        if ii > -1:
+            _obj = objects[ii]
+        else:
+            _obj = None
+        return _obj
     else:
-        _obj = None
-    return _obj
+        return None
 
 
 # todo unite all this mess into one class in order to check events analyzing only one message for all cases
@@ -71,15 +74,18 @@ class ObjectDetectionEvent(TopicEvent):
                          queue_size=queue_size)
         self._object_name = object_name
         self._confidence = confidence
+        self.current_center = None
         if object_name == 'yellow_flare':
             self._confidence -= 0.3
 
+    def get_track(self):
+        return self.current_center
+
     def _trigger_fn(self, msg: ObjectsArray):
-        obj_to_num = list(enumerate([obj.name for obj in msg.objects]))
-        for i, name in obj_to_num:
-            if self._object_name == name:
-                if msg.objects[i].confidence >= self._confidence:
-                    return 1
+        _obj = get_best_object(msg.objects, self._object_name, self._confidence)
+        if _obj:
+            self.current_center = (_obj.top_left_x + _obj.bottom_right_x) // 2
+            return 1
         return 0
 
 
