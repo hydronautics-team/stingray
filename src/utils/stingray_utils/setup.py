f159679fb767643dfac7277f5050fc901f69a887
Vladislav Plotnikov <vladik1209@gmail.com>
2023-02-10 10:55:20 +0300

    wip: stingray utils to ros2

---
+++ src/utils/stingray_utils/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+package_name = 'stingray_utils'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='vladushked',
+    maintainer_email='vladik1209@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [],
+    },
+)
