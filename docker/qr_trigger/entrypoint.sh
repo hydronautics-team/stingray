3f5ae748b6aac33fbbed9bd67c6264839ab0b308
Vladislav Plotnikov <vladik1209@gmail.com>
2025-02-27 19:14:18 +0300

    refactor qr trigger dockerfile

--- docker/qr_trigger/entrypoint.sh
+++ docker/qr_trigger/entrypoint.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 set -e
 
-# Проверяем, существует ли файл setup.bash
-if [ -f install/setup.bash ]; then
-  echo "[INFO] Код уже сбилжен"
-  source install/setup.bash
-else
-  echo "[INFO] Файл install/setup.bash не найден. Запускаем сборку..."
-  # Выполняем сборку, при ошибке удаляем build, install, log и выходим
-  source "/opt/ros/jazzy/setup.bash"
-  if ! colcon build --packages-select stingray_cam stingray_launch; then
-    echo "[ERROR] Сборка завершилась с ошибкой. Удаляем build, install, log..."
-    rm -rf build install log
-    exit 1
-  fi
-  echo "[INFO] Сборка завершена успешно. Выполняем source install/setup.bash..."
-  source install/setup.bash
-fi
+source /stingray/install/setup.bash
 
 # exec "$@"
 exec ros2 launch stingray_launch zbar.launch.py
