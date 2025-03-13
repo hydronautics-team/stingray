9efac57717fa59be96a605c51d0473fd160e9ec7
Vladislav Plotnikov <vladik1209@gmail.com>
2025-03-13 15:06:12 +0800

    add delay in qr trigger

--- src/stingray_missions/stingray_missions/qr_trigger_node.py
+++ src/stingray_missions/stingray_missions/qr_trigger_node.py
@@ -1,5 +1,4 @@
 from stingray_interfaces.srv import SetTransition
-
 import time
 import rclpy
 from rclpy.node import Node
@@ -13,50 +12,52 @@ class TransitionTriggerNode(Node):
 
         self.declare_parameter(
             'transition_srv', '/stingray/services/transition')
-        self.declare_parameter(
-            'zbar_topic', '/stingray/topics/zbar')
+        self.declare_parameter('zbar_topic', '/stingray/topics/zbar')
 
-        self.transition_client = self.create_client(SetTransition, self.get_parameter(
-            'transition_srv').get_parameter_value().string_value)
+        self.transition_client = self.create_client(
+            SetTransition,
+            self.get_parameter(
+                'transition_srv').get_parameter_value().string_value
+        )
         if not self.transition_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Service not available...')
 
-        self.get_logger().info('Service avaible, waiting qr-code')
+        self.get_logger().info('Service available, waiting for QR-code')
         self.subscription = self.create_subscription(
             String,
-            self.get_parameter('zbar_topic').get_parameter_value().string_value,
+            self.get_parameter(
+                'zbar_topic').get_parameter_value().string_value,
             self.qr_callback,
-            1)
+            1
+        )
         self.saved_transition = None
-        self.msg_repeated = 0
+        self.last_sent_time = 0.0
 
     def qr_callback(self, msg: String):
-        self.get_logger().info("Got code: " + msg.data)
+        current_time = time.time()
+        # Если код совпадает с предыдущим, проверяем задержку 3 секунды
+        if self.saved_transition == msg.data:
+            if current_time - self.last_sent_time < 3:
+                self.get_logger().info("Получен тот же QR-код, ждем 3 секунды перед повторной отправкой")
+                return
+
+        # Обновляем сохранённый код и время последней отправки
         self.saved_transition = msg.data
+        self.last_sent_time = current_time
+
+        self.get_logger().info("Получен код: " + msg.data)
         self.send_request()
 
-        # if (self.saved_transition == msg.data):
-        #     self.msg_repeated += 1
-        #     if (self.msg_repeated > 5):
-        #         self.send_request()
-        #         self.msg_repeated = 0
-        #         time.sleep(5)
-        # else:
-        #     self.msg_repeated = 0
-        #     self.saved_transition = msg.data
-
     def send_request(self):
-        self.future = self.transition_client.call_async(SetTransition.Request(transition=self.saved_transition))
-        # rclpy.spin_until_future_complete(self, self.future)
-        # return self.future.result().ok
+        self.future = self.transition_client.call_async(
+            SetTransition.Request(transition=self.saved_transition)
+        )
 
 
 def main():
     rclpy.init()
-
     node = TransitionTriggerNode()
     rclpy.spin(node)
-
     node.destroy_node()
     rclpy.shutdown()
 
